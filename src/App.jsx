f91293d883e74994770e12a076e41b0363d4eb68
Angus J <ajames@unlimitedwebhosting.co.uk>
2025-10-21 16:57:05 +0100

    unmuted + added logo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function LandingPage() {
       <div className="fixed inset-0 -z-10 overflow-hidden">
         {/* Landscape (desktop/tablet) */}
         <iframe
-          src="https://player.vimeo.com/video/1129122883?autoplay=1&loop=1&muted=1&background=1"
+          src="https://player.vimeo.com/video/1129122883?autoplay=1&loop=1&background=1"
           className="hidden sm:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
           style={{
             width: '100vw',
@@ -22,7 +22,7 @@ export default function LandingPage() {
 
         {/* Portrait (mobile) */}
         <iframe
-          src="https://player.vimeo.com/video/1129126508?autoplay=1&loop=1&muted=1&background=1"
+          src="https://player.vimeo.com/video/1129126508?autoplay=1&loop=1&background=1"
           className="block sm:hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
           style={{
             width: '100vw',
